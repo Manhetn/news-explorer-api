e4f62d35db4fefe9f8ba27dd1967f0b65aa882db
Ilya Shestakov <manhetnmd@gmail.com>
2020-05-29 21:09:21 +0300

    еще одна попытка

--- app.js
+++ app.js
@@ -11,16 +11,6 @@ const app = express();
 mongoose.connect(dataBase, mongooseOptions);
 
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-
-//   next();
-// });
 
 app.use(routers);
 
