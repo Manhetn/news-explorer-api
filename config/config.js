73e8dcd799bbd82bcf81ef98d0458c287954a106
Ilya Shestakov <manhetnmd@gmail.com>
2020-05-29 21:26:36 +0300

    вернул порт

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { NODE_ENV, JWT_SECRET, PORT, DATABASE } = process.env;
 
 const secretKey = NODE_ENV === "production" ? JWT_SECRET : "secret-key";
-const port = NODE_ENV === "production" ? PORT : 8080;
+const port = NODE_ENV === "production" ? PORT : 3000;
 const dataBase =
   NODE_ENV === "production" ? DATABASE : "mongodb://localhost:27017/newsdb";
 //  для предотвращения ошибок mongoose, после обновления
