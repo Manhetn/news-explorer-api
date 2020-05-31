2bc05cbe5620d5255c495cb4c77947ca15dc99c3
Ilya Shestakov <manhetnmd@gmail.com>
2020-05-31 21:28:18 +0300

    рефакторинг мидлвары cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ["http://localhost:8080", "https://interesting-news.tk/"],
+  origin: ["localhost:8080", "https://interesting-news.tk"],
   optionsSuccessStatus: 200,
   credentials: true,
 };
