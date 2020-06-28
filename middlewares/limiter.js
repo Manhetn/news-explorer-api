14e3cd9dcca855637d7ca62c8062efa4c395651f
Ilya Shestakov <manhetnmd@gmail.com>
2020-06-28 21:57:48 +0300

    ++ лимитер

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,5 +2,5 @@ const rateLimit = require("express-rate-limit");
 // limiter
 module.exports = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 500, // limit each IP to 100 requests per windowMs
 });
